d0a99cd6d8996d9a984b571fb9165b3cfc44170e
sornesh-052 <123526385+sornesh-052@users.noreply.github.com>
2023-01-25 09:32:29 +0530

    Update fibonacci (4).cpp

--- fibonacci (4).cpp
+++ fibonacci (4).cpp
@@ -4,8 +4,8 @@ int main()
 	int n,a=0,b=1,c;
 	printf("enter the limit = ");
 	scanf("%d",&n);
-	//printf("%d\n",a);
-	//printf("%d\n",b);
+	printf("%d\n",a);
+	printf("%d\n",b);
 	while(c<=n)
 	{ 
 		c = a+b;
