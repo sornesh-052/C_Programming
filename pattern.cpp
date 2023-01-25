25d047e68114f728886a70b2b893b420bcc5b8c4
sornesh-052 <123526385+sornesh-052@users.noreply.github.com>
2023-01-25 18:56:06 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int i,j,k;
+	printf("Enter number of rows:");
+	scanf("%d",&k);
+	for (i=1;i<=k;++i)
+	{for (j=1;j<=i;++j)
+	printf("%d",i);//for other pattern printf("%d",j)
+	printf("\n");
+		
+	}
+	
+	}
---
+++ printing ele  in array.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int i;
+	int arr ['s']={10,20,30,40,50};
+	for(i=0;i<5;i++)
+	{
+		printf("value of arr[%d]is%d\n",i,arr[i]);	
+	}
+}
