ef1c368dd86388ad323384b54fb58daade9c9706
sornesh-052 <123526385+sornesh-052@users.noreply.github.com>
2023-01-25 09:47:29 +0530

    Add files via upload

---
+++ square and cube.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	float num,square,cube;
+	
+	printf("ENTER A NUMBER : ");
+	scanf("%f",&num);
+	square=num*num;
+	printf("SQUARE OF THE  NUMBER :%f\n",square);
+	cube=num*num*num;
+	printf("CUBE OF THE NUMBER :%f\n ",cube);
+	
+}
