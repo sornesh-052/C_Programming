6443c3fe81bdb24c047b5e49c40e0f77fa97d24d
sornesh-052 <123526385+sornesh-052@users.noreply.github.com>
2023-01-25 09:33:28 +0530

    Add files via upload

---
+++ perfect num.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int n,i=1,sum=0;
+	printf("Enter a number to check if it is perfect number:");
+	scanf("%d",&n);
+	while (i<n)
+	{
+	if(n%i==0)
+	sum=sum+i;
+	i=i+1;
+	}
+	if (sum==n)
+	printf("The number %d is a perfect number",n);
+	else
+	printf("The number %d is not a perfect number",n);
+	
+	
+	
+	
+	
+return 0;
+}
